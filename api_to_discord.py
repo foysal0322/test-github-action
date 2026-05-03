e76b7b2574fc0f3e7159ec999041d4fa09eed4e1
S. M. Foysal Ahmed <foysal.ahmed@ixorasolution.com>
2026-05-03 15:26:27 +0600

    Fix script to send full JSON data instead of looking for 'content' key

--- api_to_discord.py
+++ api_to_discord.py
@@ -1,7 +1,7 @@
 import requests
 
-# API URL to fetch data from (using a public API for random quotes)
-api_url = 'https://api.quotable.io/random'
+# API URL to fetch data from (using a public API for todos)
+api_url = 'https://dummyjson.com/todos'
 
 # Discord webhook URL
 webhook_url = 'https://discord.com/api/webhooks/1351086319815888916/GkSxw4XAuJDCeshqZ95GBLYiwwgk7VCv3LFL7qDsPBIqXebwBshikJd8HcJm-9OT0H6B'
@@ -12,8 +12,8 @@ try:
     response.raise_for_status()  # Raise an error for bad status codes
     data = response.json()
     
-    # Extract the quote content (assuming the API returns a JSON with 'content' key)
-    message = data.get('content', 'No content available')
+    # Send the full JSON data as a string to Discord
+    message = str(data)
     
     # Send the message to Discord webhook
     webhook_response = requests.post(webhook_url, json={'content': message})
